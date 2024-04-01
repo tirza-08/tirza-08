076537e004698967e367301cb11f711d3ee03e80
tirza-08 <126205072+tirza-08@users.noreply.github.com>
2024-04-01 14:17:46 +0200

    basis project pagina's

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,11 +1,31 @@
-<script setup>
+<script>
 import LittleStar from './icons/IconLittleStar.vue'
+import { RouterLink, RouterView } from 'vue-router'
+
+import projects from '@/data/projects.json';
+
+
+export default {
+    data() {
+        return {
+            projects: projects
+        };
+    }
+};
 </script>
 
 <template>
-    <div id="projects-container">
+    <div id="projects-background">
         <section id="projects">
-            <h3>Mijn projecten</h3>
+            <h3>Recente projecten</h3>
+            <div class="project-container">
+                <div v-for="project in projects" :key="project.id">
+                    <RouterLink :to=project.slug>
+                        <img :src="project.hoofdfoto" alt="foto project">
+                    </RouterLink>
+                    <p><b>{{ project.type }}</b> - {{ project.name }}</p>
+                </div>
+            </div>
         </section>
     </div>
 </template>
@@ -14,7 +34,30 @@ import LittleStar from './icons/IconLittleStar.vue'
 #projects {
     min-height: var(--min-section-height);
 }
-#projects-container {
+
+#projects-background {
     background-color: var(--dark-blue);
+}
+
+.project-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    gap: 30px;
+    width: 100%;
+    color: white;
+}
+
+.project-container div {
+    width: 40%;
+}
+
+@media (max-width: 650px) {
+    .project-container {
+        flex-direction: column;
+    }
+    .project-container div {
+        width: 100%
+    }
 }
 </style>
---
+++ src/views/ProjectView.vue
@@ -0,0 +1,12 @@
+<template>
+    <section>
+        <h1>Project View</h1>
+        <p>Project ID: {{ $route.params.projectId }}</p>
+    </section>
+</template>
+
+<style scoped>
+section {
+    min-height: var(--min-section-height);
+}
+</style>
